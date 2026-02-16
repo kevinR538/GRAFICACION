5ef27c837e10bfc1627bef40183574d4b5f2ebcd
Kevin Omar  Romero Lara <L24170721@culiacan.tecnm.mx>
2026-02-16 17:35:26 +0000

    lunes

---
+++ 01_lineas/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(800, 800);
+  background(180);
+}
+
+function draw() {
+  stroke(0);
+  line(30,20,185,20);
+  stroke(126);
+  line(185,20,185,175);
+  stroke(255);
+  line(185,175,30,175);
+  line(30,175,30,20);
+
+  
+}
